e0a3d3c2414739e3376355525ea2482e9fa11d0d
Saate123 <saateechindo@gmail.com>
2025-05-02 15:19:11 +0100

    footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { FaFacebookF, FaLinkedinIn, FaInstagram } from "react-icons/fa";
 function Footer() {
   return (
     <div>
-      <footer className="px-6 md:px-12 py-8 flex flex-col md:flex-row items-center justify-between text-gray-500 bg-[#171717]">
+      <footer className="px-6 md:px-12 py-8 flex flex-col md:flex-row items-center justify-center text-gray-500 bg-[#171717]">
         <div className="flex space-x-6 text-xl mb-4 md:mb-0">
           <Link to="https://www.facebook.com/share/16H5Kn7dHk/">
             <FaFacebookF className="hover:text-black cursor-pointer" />
@@ -15,17 +15,6 @@ function Footer() {
           </Link>
           <Link to="https://www.instagram.com/lindasomiari?igsh=MWw1YjRnanBteDMybw==">
             <FaInstagram className="hover:text-black cursor-pointer" />
-          </Link>
-        </div>
-        <div className="flex space-x-6 text-sm">
-          <Link to="#" className="hover:underline">
-            FAQ
-          </Link>
-          <Link to="#" className="hover:underline font-semibold">
-            Privacy Policy
-          </Link>
-          <Link to="#" className="hover:underline">
-            Email Us
           </Link>
         </div>
       </footer>
