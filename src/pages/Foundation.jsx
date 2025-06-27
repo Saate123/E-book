6c8ec4e4170e20d69ca94e8c575f631793a3ffc8
Saate123 <saateechindo@gmail.com>
2025-06-27 16:47:09 +0100

    spelling error

--- src/pages/Foundation.jsx
+++ src/pages/Foundation.jsx
@@ -76,7 +76,7 @@ function Foundation() {
           <div className="max-w-6xl mx-auto">
             <div className="bg-[#A72024] text-center text-white p-4 rounded-lg shadow mb-8">
               <h2 className="text-2xl sm:text-3xl font-bold">
-                Grout Foundation Response
+                Griot Foundation Response
               </h2>
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 mb-8">
